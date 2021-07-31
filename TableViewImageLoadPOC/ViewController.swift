70f9f703908c7df4b75e6b48477d1ca87991cc51
jeebanrawat.bitcot.com <jeevanrawat@bitcot.com>
2021-07-31 10:43:14 +0530

    Initial Commit

---
+++ TableViewImageLoadPOC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewImageLoadPOC
+//
+//  Created by BitCot Technologies on 31/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
